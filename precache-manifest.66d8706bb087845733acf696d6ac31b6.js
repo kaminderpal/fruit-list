1a957e7915978604c84a044341b2f76af55d2b32
kaminderpal <kaminderpal@livebarn.com>
2019-03-02 18:58:17 -0500

    Updates

---
+++ precache-manifest.66d8706bb087845733acf696d6ac31b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "35e8bc349c642741a3de",
+    "url": "/static/js/main.35e8bc34.chunk.js"
+  },
+  {
+    "revision": "135dac4f0995526cf5bb",
+    "url": "/static/js/2.135dac4f.chunk.js"
+  },
+  {
+    "revision": "2d2d74175a119202d8383807bb3046b0",
+    "url": "/index.html"
+  }
+];
