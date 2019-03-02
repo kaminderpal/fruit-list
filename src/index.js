4dfccf23516411dc215cfb868199897ed118566a
kaminderpal <kaminderpal@livebarn.com>
2019-03-01 23:26:04 -0500

    impl, theme provider as context api

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React , {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Home from './containers/home';
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
+const theme = {
+    primaryBackground : "teal",
+    primaryColor : "black",
+    secondaryColor : "white",
+    extraSmallScreen : "576px",
+    smallScreen : "768px",
+    mediumScreen : "992px",
+    largeScreen : "1200px;"
+};
 
 const GlobalStyles = createGlobalStyle`
     html,body{
         height : 100%
     }
+    body{
+        margin : 0;
+        padding : 0;
+        font-family : -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.25;
+    }
 `
 const app = (
-    <React.Fragment>
+    <Fragment>
         <GlobalStyles />
-        <Home />
-    </React.Fragment>
+        <ThemeProvider theme={theme}>
+            <Home />
+        </ThemeProvider>
+    </Fragment>
 );
 ReactDOM.render(app, document.getElementById('root'));
 
