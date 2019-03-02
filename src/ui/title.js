8a3a5a1908644f51ff78fa7b0fa3e25a6b137ab8
kaminderpal <kaminderpal@livebarn.com>
2019-03-01 23:25:14 -0500

    create title styled element

---
+++ src/ui/title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+export const Title = styled.h1`
+    margin: 0;
+    margin-top: 20px;
+    margin-left: 30px;
+    padding: 1px 40px; 
+    background : teal;
+    color : white;
+`
