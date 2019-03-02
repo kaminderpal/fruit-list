1461274a3ec51659b6dcf0fd2592e5a95e3ffea5
kaminderpal <kaminderpal@livebarn.com>
2019-03-01 23:24:30 -0500

    add container styled elem

---
+++ src/ui/container.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+    padding-right : 15px;
+    padding-left  : 15px;
+    display : flex;
+    margin : 0 auto;
+    @media screen and (min-width: ${ props => props.theme.extraSmallScreen } ){
+        max-width: 540px;
+    }
+    @media screen and (min-width: ${ props => props.theme.smallScreen } ){
+        max-width: 720px;
+    }
+    @media screen and (min-width: ${ props => props.theme.mediumScreen } ){
+        max-width: 960px;
+    }
+    @media screen and (min-width: ${ props => props.theme.largeScreen }){
+        max-width: 1140px;
+    }
+`
